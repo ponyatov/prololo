40024f4cfb4d4eccafd2e736132f16aa35b66af0
Dmitry Ponyatov <dponyatov@gmail.com>
2016-06-16 10:10:12 +0400

    + multiarity terms/vars
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -3,9 +3,24 @@
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
 int main() { glob_init(); return yyparse(); }
 
-Sym::Sym(string V){ val=V; }
+Sym::Sym(string T,string V) { tag=T; val=V; share(); }
+Sym::Sym(string V):Sym("sym",V) {}
+void Sym::push(Sym*o) { nest.push_back(o); }
+void Sym::share() { glob[val]=this; }
+
 string Sym::ptr() { ostringstream os; os << this; return os.str(); }
-string Sym::dump() { return "<"+val+"@"+ptr()+">"; }
+string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
+string Sym::head() { return "<"+tag+":"+val+"> @"+ptr(); }
+string Sym::dump(int depth) { string S = "\n"+pad(depth)+head();
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
+		S += (*it)->dump(depth+1);
+	return S; }
+
+Vector::Vector():Sym("vector","[]"){}
+void Vector::share(){} // unshareble
+
+Term::Term(string V):Sym("term",V){};
+Var::Var(string V):Sym("var",V){}
 
 map<string,Sym*> glob;
 void glob_init() {
--- hpp.hpp
+++ hpp.hpp
@@ -9,18 +9,28 @@
 using namespace std;
 
 struct Sym {
-	string val;
-	Sym(string);
-	virtual string dump(); string ptr();
+	string tag,val;
+	Sym(string,string); Sym(string);
+	vector<Sym*> nest; void push(Sym*);
+	virtual void share();
+	virtual string dump(int=0); virtual string head();
+	string ptr(); string pad(int);
 };
 
 extern map<string,Sym*> glob;
 extern void glob_init();
 
+struct Vector: Sym { Vector(); void share(); };
+
+struct Term: Sym { Term(string); };
+struct Var: Sym { Var(string); };
+
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
-#define TOC(C,X) { yylval.o = new C(yytext); return X; }
+#define TOC(C,X) { yylval.o = glob[yytext]; \
+	if (!yylval.o) yylval.o=new C(yytext); \
+	return X; }
 extern int yyparse();
 extern void yyerror(string);
 #include "ypp.tab.hpp"
