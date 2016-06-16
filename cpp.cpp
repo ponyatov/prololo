6ba5322910ef657524fede04cbff00a237a5be12
Dmitry Ponyatov <dponyatov@gmail.com>
2016-06-16 13:27:05 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -8,21 +8,15 @@ Sym::Sym(string V):Sym("sym",V) {}
 void Sym::push(Sym*o) { nest.push_back(o); }
 void Sym::share() { glob[val]=this; }
 
-string Sym::ptr() { ostringstream os; os << this; return os.str(); }
+string Sym::ptr() { ostringstream os; os << " @" << this; return os.str(); }
 string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
-string Sym::head() { return "<"+tag+":"+val+"> @"+ptr(); }
+string Sym::head() { return "<"+tag+":"+val+">"+ptr(); }
 string Sym::dump(int depth) { string S = "\n"+pad(depth)+head();
 	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
 		S += (*it)->dump(depth+1);
 	return S; }
 
 Vector::Vector():Sym("vector","[]"){}
-void Vector::share(){} // unshareble
-
-Term::Term(string V):Sym("term",V){};
-Var::Var(string V):Sym("var",V){}
 
 map<string,Sym*> glob;
-void glob_init() {
-
-}
+void glob_init(){}
--- hpp.hpp
+++ hpp.hpp
@@ -10,27 +10,22 @@ using namespace std;
 
 struct Sym {
 	string tag,val;
-	Sym(string,string); Sym(string);
+	Sym(string,string); Sym(string); virtual void share();
 	vector<Sym*> nest; void push(Sym*);
-	virtual void share();
-	virtual string dump(int=0); virtual string head();
-	string ptr(); string pad(int);
+	virtual string dump(int=0); virtual string head(); string pad(int);
+	virtual string ptr();
 };
 
 extern map<string,Sym*> glob;
 extern void glob_init();
 
-struct Vector: Sym { Vector(); void share(); };
-
-struct Term: Sym { Term(string); };
-struct Var: Sym { Var(string); };
+struct Vector: Sym { Vector(); };
 
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
 #define TOC(C,X) { yylval.o = glob[yytext]; \
-	if (!yylval.o) yylval.o=new C(yytext); \
-	return X; }
+	if (!yylval.o) yylval.o = new C(yytext); return X; }
 extern int yyparse();
 extern void yyerror(string);
 #include "ypp.tab.hpp"
